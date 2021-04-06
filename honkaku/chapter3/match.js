4d3f572888a2b3b267e9ed0bf98b425e2e133869
愿山聖 <asjstksmr@gmail.com>
2021-04-06 13:29:25 +0900

    [habit]

---
+++ honkaku/chapter3/match.js
@@ -0,0 +1,8 @@
+var p = /http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w- .\/?%&=]*)?/gi;
+var str = "サポートサイトはhttp://www.wings.msn.to/です";
+str += "サンプルサイトhttp://www.web-deli.com/もよろしく！";
+console.log(str);
+var result = str.match(p);
+for(var i =0, len=result.length; i<len; i++){
+	console.log(result[i]);
+}
